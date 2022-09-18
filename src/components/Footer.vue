3dd02a6dfb0d5535c154225d07053cb6504c901a
lin <1439713475@qq.com>
2022-09-18 16:19:45 +0800

    更新了footer的样式`

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -9,12 +9,12 @@
 
 <style>
 #footer {
-    margin-top: 50px;
+    margin-top: 100px;
     width: 100%;
     height: 50px;
     background-color: rgb(255, 255, 255, 0.7);
     backdrop-filter: blur(20px);
-    box-shadow: 0 0 5px rgb(0,0,0,0.3);
+    box-shadow: 0 0 10px rgb(0,0,0,0.2);
 }
 
 .footer_text {
